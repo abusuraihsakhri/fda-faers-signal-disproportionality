eb3b3ac5b6ade386d2fd62c127fc76a8cae1ce55
abusuraihsakhri <abusuraihsakhri@gmail.com>
2026-08-26 00:00:38 +0530

    Initial Commit

---
+++ faers_sentinel/__init__.py
@@ -0,0 +1,6 @@
+"""
+FAERS-Sentinel: PRR, ROR & Bayesian Information Component (IC) Agent
+Domain: Pharmacovigilance
+Standard: WHO-UMC & FDA FAERS Signal Detection
+"""
+__version__ = "2.0.0-FRONTIER"
